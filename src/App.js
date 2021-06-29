0020deebf98629771a676917f51ce86fdd681f04
Allen <jihoshin.28@gmail.com>
2021-06-28 23:29:35 -0700

    added useEffect function for getting data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React, {useState, useEffect} from 'react'
+import {getTrending} from './api'
 import './App.css';
 
 function App() {
+
+  const [movies, setMovies] = useState([])
+  const [tvShows, setTvShows] = useState([])
+
+  useEffect(() => {
+    getTrending('movie', 'day')
+    .then((res) => {
+      setMovies(res.data.results)
+      console.log(res.data.results)
+    }).catch((err) => {
+      throw err
+    })
+    
+    getTrending('tv', 'day')
+    .then((res) => {
+      setTvShows(res.data.results)
+      console.log(res.data.results)
+    }).catch((err) => {
+      throw err
+    })
+
+  },[])
+
   
   return (
     <div className="App">
