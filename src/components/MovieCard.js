142539922f863f8cda7329191526d99d92edef97
Allen <jihoshin.28@gmail.com>
2021-06-28 23:39:19 -0700

    render genres

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,34 @@ import React, {useEffect, useState} from 'react';
 import { getMovie } from '../api'
 const MovieCard = ({poster, title, rating, id}) => {
 
+    const [genres, setGenres] = useState([])
+
+    useEffect(() => {
+        getMovie(id)
+        .then((res) => {
+            setGenres(res.data.genres)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    let renderGenres = () => {
+        if(!genres){
+            return <div>Loading Genres</div>
+        } else {
+            let genreString = ''
+            genres.forEach((genre, index) => {
+                if(index !== genres.length - 1){
+                    genreString += `${genre.name}, `
+                } else {
+                    genreString += `${genre.name}`
+                }
+            })
+            return genreString
+        }
+    }
+    
     return (
         <div className = "card">
             <img className = "poster" src = {`https://image.tmdb.org/t/p/w500/${poster}`}/>
@@ -12,6 +40,11 @@ const MovieCard = ({poster, title, rating, id}) => {
                 <div className = "titleDiv">
                     <div className = "title">
                         <h6>{title}</h6>
+                    </div>
+                    <div className = "genres">
+                        <h6>
+                            {renderGenres()}
+                        </h6> 
                     </div>
                 </div> 
                 <div>
--- src/components/TvShowCard.js
+++ src/components/TvShowCard.js
@@ -3,6 +3,34 @@ import { getTvShow } from '../api'
 
 const TvShowCard = ({poster, title, rating, id}) => {
 
+    const [genres, setGenres] = useState([])
+
+    useEffect(() => {
+        getTvShow(id)
+        .then((res) => {
+            setGenres(res.data.genres)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    let renderGenres = () => {
+        if(!genres){
+            return <div>Loading Genres</div>
+        } else {
+            let genreString = ''
+            genres.forEach((genre, index) => {
+                if(index !== genres.length - 1){
+                    genreString += `${genre.name}, `
+                } else {
+                    genreString += `${genre.name}`
+                }
+            })
+            return genreString
+        }
+    }
+
     return (
         <div className = "card">
             <img className = "poster" src = {`https://image.tmdb.org/t/p/w500/${poster}`}/>
@@ -13,6 +41,11 @@ const TvShowCard = ({poster, title, rating, id}) => {
                 <div className = "titleDiv">
                     <div className = "title">
                         <h6>{title}</h6>
+                    </div>
+                    <div className = "genres">
+                        <h6>
+                            {renderGenres()}
+                        </h6> 
                     </div>
                 </div> 
                 <div>
