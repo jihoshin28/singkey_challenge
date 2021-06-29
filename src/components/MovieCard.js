654d108ccd918a92823ef8e24264d0a52222aff3
Allen <jihoshin.28@gmail.com>
2021-06-28 23:37:25 -0700

    card info w/ image

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,21 @@ const MovieCard = ({poster, title, rating, id}) => {
 
     return (
         <div className = "card">
-            
+            <img className = "poster" src = {`https://image.tmdb.org/t/p/w500/${poster}`}/>
+            <div className = "info">
+                <div className = "rating">
+                    <h5>{rating}/10</h5> 
+                </div> 
+                <div className = "titleDiv">
+                    <div className = "title">
+                        <h6>{title}</h6>
+                    </div>
+                </div> 
+                <div>
+                    <a className = "trailer-link"  href = "www.google.com">Watch Trailer</a>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/components/TvShowCard.js
+++ src/components/TvShowCard.js
@@ -5,7 +5,21 @@ const TvShowCard = ({poster, title, rating, id}) => {
 
     return (
         <div className = "card">
-            
+            <img className = "poster" src = {`https://image.tmdb.org/t/p/w500/${poster}`}/>
+            <div className = "info">
+                <div className = "rating">
+                    <h5>{rating}/10</h5> 
+                </div> 
+                <div className = "titleDiv">
+                    <div className = "title">
+                        <h6>{title}</h6>
+                    </div>
+                </div> 
+                <div>
+                    <a className = "trailer-link"  href = "www.google.com">Watch Trailer</a>
+                </div>
+            </div>
+
         </div>
     );
 };
