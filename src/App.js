b6820611e61ad1088c26107f45af0c0b2cf1126d
Allen <jihoshin.28@gmail.com>
2021-06-28 23:26:18 -0700

    initial backbone

--- src/App.js
+++ src/App.js
@@ -1,61 +1,7 @@
 import React, {useState, useEffect} from 'react'
-import {getTrending} from './api'
-import MovieCard from './components/MovieCard'
-import TvShowCard from './components/TvShowCard'
-import LazyLoad from './components/LazyLoad'
 import './App.css';
 
 function App() {
-
-  const [movies, setMovies] = useState([])
-  const [tvShows, setTvShows] = useState([])
-
-  useEffect(() => {
-    getTrending('movie', 'day')
-    .then((res) => {
-      setMovies(res.data.results)
-      console.log(res.data.results)
-    }).catch((err) => {
-      throw err
-    })
-    
-    getTrending('tv', 'day')
-    .then((res) => {
-      setTvShows(res.data.results)
-      console.log(res.data.results)
-    }).catch((err) => {
-      throw err
-    })
-
-  },[])
-
-  const renderMovies = () => {
-    if(!movies){
-      return <div>Loading Movies...</div>
-    } else {
-      return movies.map(({backdrop_path, original_title, vote_average, id}) => {
-        return (
-          <LazyLoad>
-            <MovieCard poster = {backdrop_path} title = {original_title} rating = {vote_average} id = {id}/>
-          </LazyLoad>
-        )
-      })
-    }
-  }
-
-  const renderTvShows = () => {
-    if(!tvShows){
-      return<div>Loading Shows...</div> 
-    } else {
-      return tvShows.map(({backdrop_path, name, vote_average, id}) => {
-        return (
-          <LazyLoad>
-            <TvShowCard poster = {backdrop_path} title = {name} rating = {vote_average} id = {id}/>
-          </LazyLoad>
-        )
-      })
-    }
-  }
   
   return (
     <div className="App">
@@ -67,14 +13,6 @@ function App() {
           <h3>Trending Tv Shows</h3>
         </div>
       </div>
-      <div className = "display-section">
-        <div class = 'movie-display'>
-          {renderMovies()}
-        </div>
-        <div class = 'tv-display'>
-          {renderTvShows()}
-        </div>
-      </div> 
     </div>
   );
 }
